f70956111d682d0cec4dee8459ec9cde097f3cc8
ThearithL <bouthearith0@gmail.com>
2025-09-16 16:50:07 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -18,3 +18,7 @@ export default function Dta() {
     <div><Data/></div>
   )
 }
+
+
+
+
--- src/component/Data.jsx
+++ src/component/Data.jsx
@@ -1,14 +1,108 @@
-import React from 'react'
+// import React from 'react'
+
+// export default function Data() {
+//     const user = ['Nika','Thida','Nita','Raksmey']
+//   return (
+//     <> 
+//         {user.map((item , index)=> (
+//             <h1 className='text-3xl font-medium'>
+//                 user{index+1} : {item}
+//             </h1>
+//         ))}
+//     </>
+//   )
+// }
+
+
+
+
+
+import React from 'react';
 
 export default function Data() {
-    const user = ['Nika','Thida','Nita','Raksmey']
+  const user = [
+    {
+      name: 'Nika',
+      gender: 'female',
+      age: 18,
+      Province: 'kompong thom',
+      school: 'RUPP',
+      email: 'Jonh14@gmail.com'
+    },
+    {
+      name: 'Vannak',
+      gender: 'male',
+      age: 21,
+      Province: 'phnom penh',
+      school: 'UC',
+      email: 'vannak.uc@example.com'
+    },
+    {
+      name: 'Sokha',
+      gender: 'female',
+      age: 20,
+      Province: 'Battambang',
+      school: 'ITC',
+      email: 'sokha.itc@example.com'
+    },
+    {
+      name: 'Dara',
+      gender: 'male',
+      age: 19,
+      Province: 'siem reap',
+      school: 'NU',
+      email: 'dara.nu@example.com'
+    },
+    {
+      name: 'Srey Leak',
+      gender: 'female',
+      age: 22,
+      Province: 'Kampot',
+      school: 'NUM',
+      email: 'sreyleak.num@example.com'
+    },
+    {
+      name: 'Sok Heng',
+      gender: 'male',
+      age: 23,
+      Province: 'Takeo',
+      school: 'RUPP',
+      email: 'sokheng.rupp@example.com'
+    },
+    {
+      name: 'Channak',
+      gender: 'male',
+      age: 20,
+      Province: 'Kandal',
+      school: 'UC',
+      email: 'channak.uc@example.com'
+    }
+  ];
+
   return (
-    <> 
-        {user.map((item , index)=> (
-            <h1 className='text-3xl font-medium'>
-                user{index+1} : {item}
-            </h1>
-        ))}
+    <>
+      <div className='w-full '>
+        <table className='w-full'>
+          <tr className='text-start'>
+            <th>NAME</th>
+            <th>GENDER</th>
+            <th>AGE</th>
+            <th>PROVINCE</th>
+            <th>SCHOOL</th>
+            <th>EMAIL</th>
+          </tr>
+          {user.map((item, index) => (
+            <tr key={index} className='border-2 border-black text-center hover:bg-gray-300 '>
+              <td>{item.name}</td>
+              <td>{item.gender}</td>
+              <td>{item.age}</td>
+              <td>{item.Province}</td>
+              <td>{item.school}</td>
+              <td>{item.email}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
     </>
-  )
+  );
 }
