6f8fdc3257c8882a9887e1d36f4f1120f2f9ee58
ThearithL <bouthearith0@gmail.com>
2025-09-17 12:18:40 +0700

    update

--- src/component/State.jsx
+++ src/component/State.jsx
@@ -4,10 +4,14 @@ import { useState } from 'react'
 export default function State() {
     const [Number , setNumber] = useState(10)
   return (
-    <div className='text-center'>
-        <h1 className='text-3xl'>{Number}</h1>
-        <button onClick={()=> setNumber(Number+1)} className='text-5xl bg-green-500  m-3'>+1</button > <br/>
+    <div className='w-full h-100vh'>
+        <div>
+          <h1 className='text-3xl'>{Number}</h1>
+        </div>
+      <div>
+                <button onClick={()=> setNumber(Number+1)} className='text-5xl bg-green-500  m-3'>+1</button > <br/>
         <button onClick={()=> setNumber(Number-1)} className='text-5xl bg-red-500 '>-1</button>
+      </div>
     </div>
   )
 }
